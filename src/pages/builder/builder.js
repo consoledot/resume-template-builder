a4df0caefc1ab0908afc32421b87462a59b3ae3e
consoledot <bimbooladele@outlook.com>
2021-01-22 14:01:56 +0100

    changed styles to modules

--- src/pages/builder/builder.js
+++ src/pages/builder/builder.js
@@ -1,6 +1,6 @@
 const Builder =()=>(
-    <div>
-        Build
+    <div className="header">
+       <h1>Fill in your infomations in the space provided below to Get Started</h1>
     </div>
 )
 export default Builder
--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,4 +1,4 @@
-import "./homepage.style.css";
+import styles from "./homepage.module.css";
 import { ReactComponent as Logo } from "../../assest/hompage-image.svg";
 import Header from '../../component/header/header.component'
 import {Link} from 'react-router-dom'
@@ -6,17 +6,17 @@ import {Link} from 'react-router-dom'
 const HomePage = () => (
   <>
   <Header/>
-  <div className="homepage">
-    <div className="left">
-      <Logo className="homepage-img" />
+  <div className={styles.homepage}>
+    <div className={styles.left}>
+      <Logo className={styles.homepage__img}/>
     </div>
-    <div className="right">
-      <h2 className="home-title">Resume Builder(and CV Formate)</h2>
+    <div className={styles.right}>
+      <h2 className={styles.home__title}>Resume Builder(and CV Formate)</h2>
       <h3>  Create Dream and Aspiration for a Better Job</h3>
-      <p className="welcome-note">
+      <p className={styles.welcome__note}>
       
       </p>
-      <Link to="/builder">Get Started</Link>
+      <a href="/builder">Get Started</a>
     </div>
   </div>
   </>
